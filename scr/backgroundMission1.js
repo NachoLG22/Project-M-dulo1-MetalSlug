8480d240cfe02eb5eeadd6920124b7b87caade87
Nacho Lorente <ignaciolorentegarcia@gmail.com>
2022-12-15 20:13:08 +0100

    Estructura

---
+++ scr/backgroundMission1.js
@@ -0,0 +1,24 @@
+class Background {
+    constructor (ctx) {
+        this.ctx = ctx
+
+        this.x = 0
+        this.y = 0
+        this.w = this.ctx.canvas.width
+        this.h = this.ctx.canvas.height
+        this.vx = 0
+        this.vy = 0
+
+
+        this.img = new Image()
+        this.img.scr = "./images/background-forest.jpg"
+    }
+
+    draw() {
+        this.ctx.drawImage (this.img, this.x, this.y, this.w, this.h)
+    }
+
+
+
+}
+
--- scr/script.js
+++ scr/script.js
@@ -1,2 +1,4 @@
 const canvas  = document.getElementById("game")
 const ctx = canvas.getContext("2d")
+
+const game = new Game(ctx)
